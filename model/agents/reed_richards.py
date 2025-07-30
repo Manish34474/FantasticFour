da9a2d570291e9bab85cb84c3874b02fc09e7bb9
Manish Sah <manishsah3459@gmail.com>
2025-07-30 13:12:00 +0545

    Added Reed Richards Agent

---
+++ model/agents/reed_richards.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from model.agents.agent import Agent
+from model.environment import Environment
+from model.agents.agent import AgentRole
+from controller.config.hero_config import ReedRichardConfig as CONFIG
+from controller.config.config import Config as WorldConfig
+
+
+if TYPE_CHECKING:
+    from model.location import Location
+    from model.actions.action import Action
+
+
+class ReedRichards(Agent):
+    def __init__(self, location: Location) -> None:
+        super().__init__(location, role=AgentRole.HERO)
+        self.repair_rate = CONFIG.bridge_repair_rate
+        self.scan_range = CONFIG.scan_radius
+        self.damage_rate = CONFIG.damage_rate
+
+
+    def actions(self, environment: Environment) -> list[Optional[Action]]:
+        """
+        Define the actions that Reed Richards can perform in the environment.
+        This method should return a list of actions available to this agent.
+        
+        :param environment: The environment in which the agent operates.
+        :return: A list of actions that this agent can perform.
+        """
+
+        actionable_locations = environment.get_adjacent_locations(self._location)
+        
+        actions = []
+
+        "TODO: Find out the possible actions for Reed Richards in the current environment"
+
+        return actions
